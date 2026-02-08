18bed9147d69b0253576dba24a7ab72f391f6be2
mikaela thompson <mikaelathompson23@gmail.com>
2026-02-08 17:46:59 -0500

    Primary color fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
 /* MAIN HOME PAGE */
 
 import Image from "next/image"
+import { CircleFadingArrowUpIcon } from "lucide-react";
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <main>
-      <Button>hi this is a button!</Button>
+          <div className="flex flex-wrap items-center gap-2 md:flex-row">
+            <Button>Default</Button>
+            <Button variant="outline">Outline</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="ghost">Ghost</Button>
+            <Button variant="destructive">Destructive</Button>
+            <Button variant="link">Link</Button>
+            <Button variant="outline" size="icon">
+                  <CircleFadingArrowUpIcon />
+            </Button>
+            <Button>default</Button>
+          </div>
     </main>
+
   );
 }
