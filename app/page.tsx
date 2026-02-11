26205b47fb0b99409c1e0b4c49be2f38d96ccf4a
mikaela thompson <mikaelathompson23@gmail.com>
2026-02-11 15:25:04 -0500

    fixed primary

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <main>
+      
           <div className="flex flex-wrap items-center gap-2 md:flex-row">
             <Button>Default</Button>
             <Button variant="outline">Outline</Button>
