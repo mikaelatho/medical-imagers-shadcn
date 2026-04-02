3f263bb2639e1d3f8a742b0d20cc848fb82fad04
enzoherce <vherce@purdue.edu>
2026-04-02 15:12:32 -0400

    nav bar links fix

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -25,12 +25,12 @@ import Link from 'next/link';
 
 const ListItem = forwardRef<
   HTMLAnchorElement,
-  React.ComponentPropsWithoutRef<"a"> & { title: string }
+  React.ComponentPropsWithoutRef<typeof Link> & { title: string }
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -42,7 +42,7 @@ const ListItem = forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   );
@@ -54,7 +54,7 @@ export function Nav() {
   return (
     <nav className="flex w-full items-center justify-between px-6 md:px-20 py-4 border-b font-inter">
       {/* LEFT: Logo/Title */}
-      <h1 className="text-lg font-bold [text-shadow:0_1px_6px_#ffff]"><a href="/">LearnMRI</a></h1>
+      <h1 className="text-lg font-bold [text-shadow:0_1px_6px_#ffff]"><Link href="/">LearnMRI</Link></h1>
       {/* RIGHT: Desktop Menu (Hidden on mobile) */}
       <div className="hidden md:flex items-center gap-10 relative">
         <NavigationMenu>
